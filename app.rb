68a1dcf29dbf5cb15ec65fbee5db76630936c10a
Soumya Ray <soumya.ray@gmail.com>
2014-11-03 07:07:47 +0800

    Added namespacing for sinatra base

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require 'sinatra/base'
+require 'sinatra/namespace'
 require 'codebadges'
 require 'json'
 
 ##
 # Simple version of CodeCadetApp from https://github.com/ISS-SOA/codecadet
 class CodecadetApp < Sinatra::Base
+  register Sinatra::Namespace
+
   configure :production, :development do
     enable :logging
   end
@@ -45,22 +48,24 @@ class CodecadetApp < Sinatra::Base
     'Simplecadet is up and working'
   end
 
-  get '/api/v1/cadet/:username.json' do
-    content_type :json
-    user.to_json
-  end
-
-  post '/api/v1/check' do
-    content_type :json
-    begin
-      req = JSON.parse(request.body.read)
-      logger.info req
-    rescue
-      halt 400
+  namespace '/api/v1' do
+    get '/cadet/:username.json' do
+      content_type :json
+      user.to_json
     end
 
-    usernames = req['usernames']
-    badges = req['badges']
-    check_badges(usernames, badges).to_json
+    post '/check' do
+      content_type :json
+      begin
+        req = JSON.parse(request.body.read)
+        logger.info req
+      rescue
+        halt 400
+      end
+
+      usernames = req['usernames']
+      badges = req['badges']
+      check_badges(usernames, badges).to_json
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,54 +1,59 @@
 require_relative 'spec_helper'
+require_relative 'support/story_helpers'
 require 'json'
 
-describe 'Getting the root of the service' do
-  it 'Should return ok' do
-    get '/'
-    last_response.must_be :ok?
-    last_response.body.must_match(/simplecadet/i)
-  end
-end
+describe 'SimpleCadet Stories' do
+  include StoryHelpers
 
-describe 'Getting cadet information' do
-  it 'should return their badges' do
-    get '/api/v1/cadet/soumya.ray.json'
-    last_response.must_be :ok?
+  describe 'Getting the root of the service' do
+    it 'Should return ok' do
+      get '/'
+      last_response.must_be :ok?
+      last_response.body.must_match(/simplecadet/i)
+    end
   end
 
-  it 'should return 404 for unknown user' do
-    get "/api/v1/cadet/#{random_str(20)}.json"
-    last_response.must_be :not_found?
-  end
-end
+  describe 'Getting cadet information' do
+    it 'should return their badges' do
+      get '/api/v1/cadet/soumya.ray.json'
+      last_response.must_be :ok?
+    end
 
-describe 'Checking users for badges' do
-  it 'should find missing badges' do
-    header = { 'CONTENT_TYPE' => 'application/json' }
-    body = {
-      usernames: ['soumya.ray', 'chenlizhan'],
-      badges: ['Object-Oriented Programming II']
-    }
-
-    post '/api/v1/check', body.to_json, header
-    last_response.must_be :ok?
+    it 'should return 404 for unknown user' do
+      get "/api/v1/cadet/#{random_str(20)}.json"
+      last_response.must_be :not_found?
+    end
   end
 
-  it 'should return 404 for unknown users' do
-    header = { 'CONTENT_TYPE' => 'application/json' }
-    body = {
-      usernames: [random_str(15), random_str(15)],
-      badges: [random_str(30)]
-    }
+  describe 'Checking users for badges' do
+    it 'should find missing badges' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        usernames: ['soumya.ray', 'chenlizhan'],
+        badges: ['Object-Oriented Programming II']
+      }
 
-    post '/api/v1/check', body.to_json, header
-    last_response.must_be :not_found?
-  end
+      post '/api/v1/check', body.to_json, header
+      last_response.must_be :ok?
+    end
 
-  it 'should return 400 for bad JSON formatting' do
-    header = { 'CONTENT_TYPE' => 'application/json' }
-    body = random_str(50)
+    it 'should return 404 for unknown users' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        usernames: [random_str(15), random_str(15)],
+        badges: [random_str(30)]
+      }
 
-    post '/api/v1/check', body, header
-    last_response.must_be :bad_request?
+      post '/api/v1/check', body.to_json, header
+      last_response.must_be :not_found?
+    end
+
+    it 'should return 400 for bad JSON formatting' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = random_str(50)
+
+      post '/api/v1/check', body, header
+      last_response.must_be :bad_request?
+    end
   end
 end
