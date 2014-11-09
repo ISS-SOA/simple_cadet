5f081187671c38f6a76a2b4938f2d7573d1483ef
Soumya Ray <soumya.ray@gmail.com>
2014-11-09 20:02:55 +0800

    Added schema to migration

--- db/migrate/20141109114035_create_tutorials.rb
+++ db/migrate/20141109114035_create_tutorials.rb
@@ -1,4 +1,14 @@
 class CreateTutorials < ActiveRecord::Migration
-  def change
+  def self.up
+    create_table :tuturials do |t|
+      t.string :name
+      t.text :usernames
+      t.text :badges
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tutorials
   end
 end
